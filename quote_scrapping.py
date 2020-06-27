3687c80015b8cbbcb3b5294e57fc40240478cee2
MonicaBing <47180942+MonicaBing@users.noreply.github.com>
2020-06-27 10:58:56 +0100

    Add files via upload

---
+++ quote_scrapping.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 10:23:39 2020
+
+"""
+
+from bs4 import BeautifulSoup as bs 
+import pandas as pd
+pd.set_option('display.max_colwidth',500)
+import time 
+import requests
+import random
+
+
+urls = [f"http://quotes.toscrape.com/page/{i}/" for i in range (1,11)]
+
+rate = [i/10 for i in range (10)]
+
+quotes = []
+authors = []
+
+
+for url in urls:
+    page = requests.get(url)
+    soup = bs(page.content)
+    authors.extend([i.text for i in soup.find_all(class_='author')])
+    quotes.extend([i.text for i in soup.find_all(class_='text')])
+    
+    if len(quotes) >= 52:
+        break
+    time.sleep(random.choice(rate))
+
+df = pd.DataFrame()
+df['Authors'] = authors
+df['Quotes'] = quotes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
